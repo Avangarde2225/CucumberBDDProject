cb33fd355d45b998a0c78c9b675ae04abf037144
Serdar <serdar2225@gmail.com>
2020-06-25 09:22:44 -0400

    citizenshipPage methods

--- src/main/java/PageObjectModel/citizenshipPage.java
+++ src/main/java/PageObjectModel/citizenshipPage.java
@@ -1,10 +1,12 @@
 package PageObjectModel;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import utilities.Driver;
 
-public class citizenshipPage {
+public class citizenshipPage extends abstractClass{
     private WebDriver driver;
 
     public citizenshipPage(){
@@ -12,4 +14,16 @@ public class citizenshipPage {
         PageFactory.initElements(driver,this);
     }
 
+    @FindBy(xpath="(//ms-text-field[@placeholder=\"GENERAL.FIELD.NAME\"])[1]")
+    private WebElement inputCitizenName;
+
+    @FindBy(xpath="(//ms-text-field[@placeholder=\"GENERAL.FIELD.SHORTNAME\"])[1]")
+    private WebElement inputShortName;
+
+    public void typeInTheInputCitizenName(String name){
+        sendKeys(inputCitizenName, name);
+    }
+    public void typeInTheInputShortName(String name){
+        sendKeys(inputShortName, name);
+    }
 }
