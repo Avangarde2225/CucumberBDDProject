d31c8a6d87b72c071b36e2166dfe7d35314bdd49
Serdar <serdar2225@gmail.com>
2020-06-26 12:07:27 -0400

    one step ahead to complete the class

--- src/main/java/stepDefinition/discountSteps.java
+++ src/main/java/stepDefinition/discountSteps.java
@@ -1,11 +1,13 @@
 package stepDefinition;
 
+import PageObjectModel.discountPage;
 import PageObjectModel.leftNav;
 import cucumber.api.java.en.Given;
 
 public class discountSteps {
 
     leftNav lv = new leftNav();
+    discountPage dp = new discountPage();
 
 
     @Given("^Click on Discount$")
@@ -14,17 +16,17 @@ public class discountSteps {
     }
 
     @Given("^Type in the Description \"([^\"]*)\"$")
-    public void type_in_the_Description(String arg1) throws Throwable {
-
+    public void type_in_the_Description(String description) throws Throwable {
+    dp.typeInthedescription(description);
     }
 
     @Given("^Type in the IntegrationCode \"([^\"]*)\"$")
-    public void type_in_the_IntegrationCode(String arg1) throws Throwable {
-
+    public void type_in_the_IntegrationCode(String intCode) throws Throwable {
+    dp.typeInIntegrationCode(intCode);
     }
 
     @Given("^Type in the Priority \"([^\"]*)\"$")
-    public void type_in_the_Priority(String arg1) throws Throwable {
-
+    public void type_in_the_Priority(String prio) throws Throwable {
+    dp.typeIninputPriority(prio);
     }
 }
