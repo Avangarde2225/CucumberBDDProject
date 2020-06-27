018d8b190d40d5ed99b51eba02bbc47227f56220
Serdar <serdar2225@gmail.com>
2020-06-27 17:26:07 -0400

    excel workbook, sheet , row and cell data reading

--- src/main/java/utilities/myExcel.java
+++ src/main/java/utilities/myExcel.java
@@ -1,5 +1,28 @@
 package utilities;
 
+import org.apache.poi.ss.usermodel.*;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
 public class myExcel {
+
+    public static void main(String[] args) throws IOException {
+
+        String path="src/test/java/excelFiles/releaseNotes.xlsx";
+
+        FileInputStream instream = new FileInputStream(path);
+
+        Workbook workbook = WorkbookFactory.create(instream);
+
+        Sheet sheet = workbook.getSheet("Sheet1");
+
+        Row row = sheet.getRow(0);
+
+        Cell cell = row.getCell(0);
+
+        System.out.println(cell);
+    }
 
 }
